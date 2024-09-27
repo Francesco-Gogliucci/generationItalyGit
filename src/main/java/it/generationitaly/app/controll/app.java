e21a9fa63ea1083703d83262b35d9af4f0eb68c0
Francesco <fgogliucci@gmail.com>
2024-09-27 15:42:59 +0200

    hello world

--- src/main/java/it/generationitaly/app/controll/app.java
+++ src/main/java/it/generationitaly/app/controll/app.java
@@ -3,8 +3,7 @@ package it.generationitaly.app.controll;
 public class app {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
 	}
 
 }
